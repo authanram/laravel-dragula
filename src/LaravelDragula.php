44ef00084a86cb728eb09fc10d86173cb4982129
Daniel Seuffer <authanram@gmail.com>
2022-10-02 17:07:31 +0200

    WIP

--- src/LaravelDragula.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Authanram\LaravelDragula;
-
-class LaravelDragula
-{
-}
--- src/LaravelDragulaServiceProvider.php
+++ src/LaravelDragulaServiceProvider.php
@@ -4,22 +4,14 @@ namespace Authanram\LaravelDragula;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Authanram\LaravelDragula\Commands\LaravelDragulaCommand;
 
 class LaravelDragulaServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-dragula')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-dragula_table')
-            ->hasCommand(LaravelDragulaCommand::class);
+            ->hasViews();
     }
 }
